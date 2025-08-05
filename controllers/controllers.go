8a29a89023360a222fa0b6b957a2649909c27e7b
lopeslyra10 <augustolopeslyra@gmail.com>
2025-08-04 23:56:23 -0300

    Implementando um roteador (gorilla mux) para realizar o filtro por Id

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,11 +1,17 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/lopeslyra/go-api-rest/models"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home Page!")
+}
+
+func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Personalidades)
 }
--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/lopeslyra/go-api-rest/models"
 	"github.com/lopeslyra/go-api-rest/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Personalidade 1", Historia: "Historia 1"},
+		{Nome: "Personalidade 2", Historia: "Historia 2"},
+	}
+
 	fmt.Println("Server starting on port 8000...")
 	routes.HandleRequests()
 }
